57f43708a136fe994e48b4fc01fecec8d89e304c
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2022-01-16 19:58:04 +0530

    Updates

---
+++ precache-manifest.b6e4f7172c3912ccc561a2e725f53047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41724eee0ca82018debb76196a58e22e",
+    "url": "/textutils2/index.html"
+  },
+  {
+    "revision": "386cc66dfd160295eed7",
+    "url": "/textutils2/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/textutils2/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/textutils2/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "386cc66dfd160295eed7",
+    "url": "/textutils2/static/js/main.153cc5fb.chunk.js"
+  },
+  {
+    "revision": "36a092556cb246e6be0a",
+    "url": "/textutils2/static/js/runtime~main.f7672452.js"
+  }
+]);
